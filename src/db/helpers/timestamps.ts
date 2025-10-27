cc1dc32f1105986ae794fea376ff12ab297a6efc
Ryan Luong <lumintriet@gmail.com>
2025-10-27 11:37:52 +0700

    refactor(db): add timestamp helper for identity tables. Add default now to accounts and sessions tables

---
+++ src/db/helpers/timestamps.ts
@@ -0,0 +1,11 @@
+import { timestamp } from "drizzle-orm/pg-core";
+
+const timestamps = {
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdate(() => /* @__PURE__ */ new Date())
+    .notNull(),
+};
+
+export { timestamps };
