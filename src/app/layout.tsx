9b30883dbe83bb3eaa1a7850ed98d70b716ee718
Ryan Luong <lumintriet@gmail.com>
2025-10-27 10:00:38 +0700

    feat(ui): add a mode toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/app/globals.css";
 import { Providers } from "@/components/providers";
+import { ModeToggle } from "@/components/utils/mode-toggle";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>{children}</Providers>
+        <Providers>
+          {children}
+          <ModeToggle className="fixed top-3 right-3 z-50" />
+        </Providers>
       </body>
     </html>
   );
