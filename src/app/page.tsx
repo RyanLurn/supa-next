66229e25f6d40e270a2b4faf765c56ea62346581
Ryan Luong <lumintriet@gmail.com>
2025-10-27 09:56:22 +0700

    chore(ui): add button and dropdown-menu components from shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-center gap-y-4">
       <h1 className="text-4xl font-bold">Supa Next</h1>
       <p className="text-lg">A Next.js app template with a Supabase backend</p>
+      <Button>Button</Button>
     </main>
   );
 }
