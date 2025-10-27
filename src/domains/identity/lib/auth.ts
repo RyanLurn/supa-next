2a2a7236ceb7090578e66cb1720e128c0ee117bf
Ryan Luong <lumintriet@gmail.com>
2025-10-27 10:53:46 +0700

    chore(db): create identity tables in database

--- src/domains/identity/lib/auth.ts
+++ src/domains/identity/lib/auth.ts
@@ -6,6 +6,7 @@ import { serverEnvVars } from "@/env/server";
 const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
+    usePlural: true,
   }),
   secret: serverEnvVars.BETTER_AUTH_SECRET,
   baseURL: serverEnvVars.BETTER_AUTH_URL,
