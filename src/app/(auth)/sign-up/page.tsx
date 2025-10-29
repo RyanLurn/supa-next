3256ce6a94781685e012fcbb1be1903f4af08a3c
Ryan Luong <lumintriet@gmail.com>
2025-10-29 09:55:09 +0700

    feat(identity): sign up and sign in page UI

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { GoogleSignInButton } from "@/domains/identity/auth/components/oauth/google-sign-in-button";
+
+export default function SignUpPage() {
+  return (
+    <div className="h-full flex flex-col items-center justify-center gap-y-5">
+      <h1 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+        Sign up
+      </h1>
+      <GoogleSignInButton />
+      <p className="text-center">
+        Already have an account?{" "}
+        <Button variant="link" asChild className="p-0 ml-1">
+          <Link href="/sign-in">Sign in</Link>
+        </Button>
+      </p>
+    </div>
+  );
+}
