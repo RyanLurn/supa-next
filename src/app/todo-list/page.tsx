a296b0302cd8b94267e20305e2774d7d97b444d3
Ryan Luong <lumintriet@gmail.com>
2025-11-01 07:55:32 +0700

    feat: display todo list

--- src/app/todo-list/page.tsx
+++ src/app/todo-list/page.tsx
@@ -1,19 +1,40 @@
+import { eq } from "drizzle-orm";
+import { db } from "@/db/connect";
+import { tasks } from "@/db/schema/todo-list";
 import { UserButton } from "@/domains/identity/account/components/button/user-button";
 import { getUser } from "@/domains/identity/helpers/get-user";
+import { TaskItem } from "@/domains/todo-list/components/task-item";
 
 export default async function TodoListPage() {
   const authResult = await getUser();
-
   if (authResult.isErr()) {
     throw new Error(authResult.error.message);
   }
-
   const user = authResult.value;
 
-  return (
-    <div>
-      <h1>Welcome {user.name}</h1>
-      <UserButton className="fixed top-4 right-4 z-50" />
-    </div>
-  );
+  try {
+    const tasksList = await db
+      .select()
+      .from(tasks)
+      .where(eq(tasks.userId, user.id));
+
+    return (
+      <div className="size-full flex flex-col items-center">
+        <UserButton className="fixed top-4 right-4 z-50" />
+        <h1 className="text-2xl font-bold mt-12">Your tasks</h1>
+        <div className="flex flex-col gap-4">
+          {tasksList.map((task) => (
+            <TaskItem
+              key={task.id}
+              title={task.name}
+              completed={task.completed}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  } catch (error) {
+    console.error("Failed to get tasks. Unexpected error occurred:", error);
+    throw new Error("Failed to get tasks.");
+  }
 }
